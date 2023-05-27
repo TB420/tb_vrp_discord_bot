b180f8150d8d8b8eda3ec441880e22bb7032e64a
TB <95184722+TB420@users.noreply.github.com>
2023-05-27 16:11:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 exports.settings = {
-    Level1Perm: "1095821615213854850",
-    Level2Perm: "1095821381389795389 ", // Change these to the role names of the relevant permission!
-    Level3Perm: "1095819071800479815",
-    guildid: "1095817057523740682",
+    Level1Perm: "",
+    Level2Perm: "", // Change these to the role names of the relevant permission!
+    Level3Perm: "",
+    guildid: "",
     StatusEnabled: true,
-    StatusChannel: "1105920806330052688",
-    ServerIP: "coming soon",
+    StatusChannel: "",
+    ServerIP: "",
     token: "",
-    botStatus: "Watching Over Gotham City",
+    botStatus: "",
 }
